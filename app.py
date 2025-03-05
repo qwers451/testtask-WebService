ee222c95050525698696e93b88060503b4595303
qwers451 <mikhailzametaev@gmail.com>
2025-03-05 18:20:36 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    name = request.args.get('name', 'Recruto')
+    message = request.args.get('message', 'Давай дружить')
+
+    response = f"Hello {name}! {message}!"
+    return response
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
